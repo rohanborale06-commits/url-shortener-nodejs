cd43cd27d5ec9e3d1206b16ae88cfb7a7eeb2233
Rohan Borale <rohanborale06@gmail.com>
2026-03-05 11:19:45 +0530

    fix duplicate code

--- server.js
+++ server.js
@@ -4,26 +4,26 @@ const shortid = require("shortid");
 const cors = require("cors");
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
+
 app.get("/", (req, res) => {
   res.send("URL Shortener API is running 🚀");
 });
 
-// MongoDB connect
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(err));
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
 
-// Schema
 const Url = mongoose.model("Url", {
   longUrl: String,
   shortCode: String
 });
 
-// Create short URL
 app.post("/shorten", async (req, res) => {
   const { longUrl } = req.body;
+
   const shortCode = shortid.generate();
 
   const newUrl = new Url({
@@ -33,22 +33,11 @@ app.post("/shorten", async (req, res) => {
 
   await newUrl.save();
 
-  res.json({
-    shortUrl: `http://localhost:5000/${shortCode}`
-  });
+  res.json({ shortUrl: shortCode });
 });
 
-// Redirect
-app.get("/:code", async (req, res) => {
-  const url = await Url.findOne({ shortCode: req.params.code });
+const PORT = process.env.PORT || 5000;
 
-  if (url) {
-    return res.redirect(url.longUrl);
-  }
-
-  res.send("URL not found");
-});
-
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
